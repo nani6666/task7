c58fa6431eb940ff98258c26a4fc9084b0aa1189
nani6666 <raviteja.m6666@gmail.com>
2020-03-15 22:42:08 +0530

    code refactored

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 import './style.css';
 
 // Write Javascript code!
-var acctData = [
+const acctData = [
  {
  "acctNum": "AAA - 1234",
  "user": "Alice"
@@ -20,27 +20,56 @@ var acctData = [
  "user": "Alice"
  }
 ];
-var balance = {
+const balance = {
  "AAA - 1234": 4593.22,
  "AAA - 9921": 0,
  "AAA - 5231": 232142.5,
  "AAA - 8191": 4344
 };
 
-console.log(sortKey(acctData , "user" , "asc"));
-console.log(sortKey(acctData , "acctNum" , "asc"));
+console.log("Sort By User",sortByKey(acctData , "user" , "asc"));
+
+console.log("filtered by Bob -", filterArray(acctData ,"user", "Bob"));
+console.log("filtered by Charlie -", filterArray(acctData ,"user", "Charlie"));
+console.log("sorted by acctNum",sortByKey(acctData , "acctNum" , "asc"));
+console.log("filtered by Alice -", filterArray(acctData ,"user", "Alice"));
+
 
 function sortByKey(array, key , order) {
-  if (order == "asc"){
-     return array.sort(function(a, b) {
-        var x = a[key]; var y = b[key];
-        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
-  } else {
-    return array.sort(function(b, a) {
-        var x = a[key]; var y = b[key];
-        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
-    });
-  }
-    
+  return array.sort(function(a, b){
+     let textA = a[key].toUpperCase();
+     let  textB = b[key].toUpperCase();
+    if(textA < textB) { return -1; }
+    if(textA > textB) { return 1; }
+});    
+}
+
+function compareValues(key, order = 'asc') {
+  return function innerSort(a, b) {
+    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
+      // property doesn't exist on either object
+      return 0;
+    }
+
+    const varA = (typeof a[key] === 'string')
+      ? a[key].toUpperCase() : a[key];
+    const varB = (typeof b[key] === 'string')
+      ? b[key].toUpperCase() : b[key];
+
+    let comparison = 0;
+    if (varA > varB) {
+      comparison = 1;
+    } else if (varA < varB) {
+      comparison = -1;
+    }
+    return (
+      (order === 'desc') ? (comparison * -1) : comparison
+    );
+  };
+}
+
+
+function filterArray(array, key ,value){
+ let response = array.filter(data => data[key] == value);
+   return response;
 }
