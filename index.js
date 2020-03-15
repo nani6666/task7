cf54d9d55ff0672f2c4570e55e0c843b52a83831
nani6666 <raviteja.m6666@gmail.com>
2020-03-15 22:01:32 +0530

    sorting functionality done

--- index.js
+++ index.js
@@ -2,3 +2,45 @@
 import './style.css';
 
 // Write Javascript code!
+var acctData = [
+ {
+ "acctNum": "AAA - 1234",
+ "user": "Alice"
+ },
+ {
+ "acctNum": "AAA - 5231",
+ "user": "Bob"
+ },
+ {
+ "acctNum": "AAA - 9921",
+ "user": "Alice"
+ },
+ {
+ "acctNum": "AAA - 8191",
+ "user": "Alice"
+ }
+];
+var balance = {
+ "AAA - 1234": 4593.22,
+ "AAA - 9921": 0,
+ "AAA - 5231": 232142.5,
+ "AAA - 8191": 4344
+};
+
+console.log(sortKey(acctData , "user" , "asc"));
+console.log(sortKey(acctData , "acctNum" , "asc"));
+
+function sortByKey(array, key , order) {
+  if (order == "asc"){
+     return array.sort(function(a, b) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+  } else {
+    return array.sort(function(b, a) {
+        var x = a[key]; var y = b[key];
+        return ((x < y) ? -1 : ((x > y) ? 1 : 0));
+    });
+  }
+    
+}
