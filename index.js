a7180692769324807a38fa332dc8e836152295cb
nani6666 <raviteja.m6666@gmail.com>
2020-03-15 22:53:38 +0530

    code refactored

--- index.js
+++ index.js
@@ -28,7 +28,6 @@ const balance = {
 };
 
 console.log("Sort By User",sortByKey(acctData , "user" , "asc"));
-console.log("sort by user new -",acctData.sort(compareValues('user', 'desc')));
 console.log("filtered by Bob -", filterArray(acctData ,"user", "Bob"));
 console.log("filtered by Charlie -", filterArray(acctData ,"user", "Charlie"));
 console.log("sorted by acctNum",sortByKey(acctData , "acctNum" , "asc"));
@@ -45,29 +44,7 @@ function sortByKey(array, key , order) {
 });    
 }
 
-function compareValues( key, order = 'asc') {
-  return function innerSort(a, b) {
-    if (!a.hasOwnProperty(key) || !b.hasOwnProperty(key)) {
-      // property doesn't exist on either object
-      return 0;
-    }
 
-    const varA = (typeof a[key] === 'string')
-      ? a[key].toUpperCase() : a[key];
-    const varB = (typeof b[key] === 'string')
-      ? b[key].toUpperCase() : b[key];
-
-    let comparison = 0;
-    if (varA > varB) {
-      comparison = 1;
-    } else if (varA < varB) {
-      comparison = -1;
-    }
-    return (
-      (order === 'desc') ? (comparison * -1) : comparison
-    );
-  };
-}
 
 
 function filterArray(array, key ,value){
